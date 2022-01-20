95b3c06e73c9265b03748e4465a86a276f805f70
Marlon Emmerich <marlonemmerich@yahoo.com>
2022-01-19 23:48:59 -0300

    feat: Implementing static User view

--- src/views/User.jsx
+++ src/views/User.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import { TextField, Button, Container, Grid } from '@mui/material';
+
 const User = () => (
-    <div>
-       User page
-    </div>
+    <Container>
+        <Grid container direction="row" justifyContent="center" alignItems="center">
+            <form>
+                <TextField
+                    type="text"
+                    label="Name"
+                    variant="outlined"
+                    sx={{ my: 2 }}
+                    />
+                    <br />
+                <TextField
+                    type="text"
+                    label="E-mail"
+                    variant="outlined"
+                    sx={{ pb: 2 }}
+                />
+                <br />
+                <Button variant="contained" color="primary">
+                    Save User
+                </Button>
+            </form>
+        </Grid>
+    </Container>
 )
 
 export default User
